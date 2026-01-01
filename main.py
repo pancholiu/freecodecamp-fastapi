4bb3eff422ee37f3199a194bb9b6b65ade70035a
Francisco Liu <pancholiu@hotmail.com>
2026-01-01 15:04:34 -0400

    3-25: Deleting Posts

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
 from random import randrange
 
@@ -25,6 +25,12 @@ def find_post(id):
             return p
 
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 def root():
     return {"message": "Welcome to my API!"}
@@ -35,7 +41,7 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict['id'] = randrange(0, 100000)
@@ -46,4 +52,22 @@ def create_posts(post: Post):
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = find_post(id)
+
+    if not post:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            f"Post with id: {id} was not found")
+
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} does not exist")
+
+    my_posts.pop(index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
