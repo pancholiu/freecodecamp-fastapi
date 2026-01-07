2843602c398a6b1915531a4d3f44046722b2f37c
Francisco Liu <pancholiu@hotmail.com>
2026-01-07 16:38:11 -0400

    8-66: Get User by ID

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
+import time
 from typing import List
-from . import models, schemas
-from .database import engine, get_db
-from fastapi import FastAPI, HTTPException, Response, status, Depends
+
+import psycopg2
+from fastapi import Depends, FastAPI, HTTPException, Response, status
+
 # To retrieve name of column. Might not be needed in latest version
 from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 
-import psycopg2
-import time
+from . import models, schemas, utils
+from .database import engine, get_db
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -98,9 +100,23 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id {id} does not exist")
+
+    return user
