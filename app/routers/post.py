71332d947809969ffead1faddefddf5d3393d7a9
Francisco Liu <pancholiu@hotmail.com>
2026-07-16 22:16:02 -0300

    Get One Post with Joins

--- app/routers/post.py
+++ app/routers/post.py
@@ -51,9 +51,18 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     return new_post
 
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = (
+        db.query(
+            models.Post,
+            func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote,
+              models.Vote.post_id == models.Post.id,
+              isouter=True)
+        .group_by(models.Post.id)
+        .filter(models.Post.id == id).first()
+    )
 
     if not post:
         raise HTTPException(status.HTTP_404_NOT_FOUND,
